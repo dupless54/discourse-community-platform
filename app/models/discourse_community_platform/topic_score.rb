8a5e1030c773383798ac9289a026630374ce5f9e
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-01 01:27:37 +0300

    FIX: rely on unique index for topic score creation

--- app/models/discourse_community_platform/topic_score.rb
+++ app/models/discourse_community_platform/topic_score.rb
@@ -7,7 +7,6 @@ module ::DiscourseCommunityPlatform
     belongs_to :community, class_name: "::DiscourseCommunityPlatform::Community"
     belongs_to :topic, class_name: "::Topic"
 
-    validates :topic_id, uniqueness: true
     validates :upvotes, :downvotes, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :score, numericality: { only_integer: true }
   end
