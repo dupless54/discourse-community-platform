e1ab2dc99c3d699921c38f502a6276ca2d8dc4d5
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 14:28:09 +0300

    feat: expose permission-safe community endpoint

---
+++ app/controllers/discourse_community_platform/communities_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module ::DiscourseCommunityPlatform
+  class CommunitiesController < ::ApplicationController
+    requires_plugin PLUGIN_NAME
+
+    def show
+      community =
+        Community.includes(:category, :owner, :member_group, :moderator_group).find_by!(slug: params[:slug])
+
+      # Do not reveal that a private/restricted community exists unless Discourse's
+      # own category permission model allows the current guardian to see it.
+      raise Discourse::NotFound unless guardian.can_see?(community.category)
+
+      render_serialized(community, CommunitySerializer, root: :community)
+    end
+  end
+end
