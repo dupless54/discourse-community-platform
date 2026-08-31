c4f325267d7d329f780394ededb9dc18098e2609
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-01 00:59:29 +0300

    DOCS: annotate topic score schema

--- app/models/discourse_community_platform/topic_score.rb
+++ app/models/discourse_community_platform/topic_score.rb
@@ -12,3 +12,24 @@ module ::DiscourseCommunityPlatform
     validates :score, numericality: { only_integer: true }
   end
 end
+
+# == Schema Information
+#
+# Table name: discourse_community_platform_topic_scores
+#
+#  id           :bigint           not null, primary key
+#  downvotes    :integer          default(0), not null
+#  hot_score    :decimal(20, 8)   default(0.0), not null
+#  score        :integer          default(0), not null
+#  upvotes      :integer          default(0), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  community_id :integer          not null
+#  topic_id     :integer          not null
+#
+# Indexes
+#
+#  idx_dcp_topic_scores_community_hot    (community_id,hot_score)
+#  idx_dcp_topic_scores_community_score  (community_id,score)
+#  idx_dcp_topic_scores_topic            (topic_id) UNIQUE
+#
