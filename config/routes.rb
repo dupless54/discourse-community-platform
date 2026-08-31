1d92529da810dbed3a70a8b6cb8c28178b10583d
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 23:46:21 +0300

    Add community management update route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ DiscourseCommunityPlatform::Engine.routes.draw do
   scope "/", defaults: { format: :json } do
     post "communities" => "communities#create"
     get "communities/:slug" => "communities#show"
+    patch "communities/:slug" => "communities#update"
+    put "communities/:slug" => "communities#update"
     post "communities/:slug/join" => "communities#join"
     delete "communities/:slug/join" => "communities#leave"
   end
