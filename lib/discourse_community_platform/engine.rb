326fc0830ebc443773b3a391a73b0f0399d2ccff
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 15:12:00 +0300

    fix: follow Discourse engine routing convention

--- lib/discourse_community_platform/engine.rb
+++ lib/discourse_community_platform/engine.rb
@@ -6,11 +6,3 @@ module ::DiscourseCommunityPlatform
     isolate_namespace DiscourseCommunityPlatform
   end
 end
-
-DiscourseCommunityPlatform::Engine.routes.draw do
-  get "/communities/:slug" => "communities#show", defaults: { format: :json }
-end
-
-Discourse::Application.routes.append do
-  mount ::DiscourseCommunityPlatform::Engine, at: "/community-platform"
-end
