6ec2289e1a6945d3ace1664b3f6d96594e652e1a
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-01 00:49:01 +0300

    FEATURE: route ranked feeds and votes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ DiscourseCommunityPlatform::Engine.routes.draw do
   scope "/", defaults: { format: :json } do
     post "communities" => "communities#create"
     get "communities/:slug" => "communities#show"
+    get "communities/:slug/topics" => "communities#topics"
     patch "communities/:slug" => "communities#update"
     put "communities/:slug" => "communities#update"
     post "communities/:slug/join" => "communities#join"
     delete "communities/:slug/join" => "communities#leave"
+    put "topics/:topic_id/vote" => "votes#update"
   end
 end
