fabc93b9cb8ee89ac86d05313415c390cc343551
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-01 00:52:37 +0300

    STYLE: load voting controls stylesheet

--- plugin.rb
+++ plugin.rb
@@ -9,6 +9,7 @@
 
 enabled_site_setting :community_platform_enabled
 register_asset "stylesheets/community-platform.scss"
+register_asset "stylesheets/community-platform-voting.scss"
 
 module ::DiscourseCommunityPlatform
   PLUGIN_NAME = "discourse-community-platform"
