b8e946b04ec7a1ecb55ef7f703e14e703ac08286
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-01 01:37:27 +0300

    FEATURE: load global popular feed

---
+++ assets/javascripts/discourse/routes/community-platform-popular.js
@@ -0,0 +1,18 @@
+import { ajax } from "discourse/lib/ajax";
+import DiscourseRoute from "discourse/routes/discourse";
+import { i18n } from "discourse-i18n";
+
+export default class CommunityPlatformPopularRoute extends DiscourseRoute {
+  async model() {
+    const payload = await ajax("/community-platform/feeds/popular.json");
+
+    return {
+      topics: payload.topics || [],
+      order: payload.order || "popular",
+    };
+  }
+
+  titleToken() {
+    return i18n("community_platform.popular.title");
+  }
+}
