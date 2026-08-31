408483e3301a2240b05f365ac8fd1333549905dc
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-01 00:59:39 +0300

    DOCS: annotate vote schema

--- app/models/discourse_community_platform/vote.rb
+++ app/models/discourse_community_platform/vote.rb
@@ -14,3 +14,22 @@ module ::DiscourseCommunityPlatform
     validates :user_id, uniqueness: { scope: :topic_id }
   end
 end
+
+# == Schema Information
+#
+# Table name: discourse_community_platform_votes
+#
+#  id           :bigint           not null, primary key
+#  value        :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  community_id :integer          not null
+#  topic_id     :integer          not null
+#  user_id      :integer          not null
+#
+# Indexes
+#
+#  idx_dcp_votes_community_topic  (community_id,topic_id)
+#  idx_dcp_votes_topic            (topic_id)
+#  idx_dcp_votes_user_topic       (user_id,topic_id) UNIQUE
+#
