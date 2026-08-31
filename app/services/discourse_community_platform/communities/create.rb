133024043ece02eca8d51e59ef2e1d0f264f1ab3
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 15:32:43 +0300

    FIX: make community creation group membership callback-safe

--- app/services/discourse_community_platform/communities/create.rb
+++ app/services/discourse_community_platform/communities/create.rb
@@ -5,6 +5,7 @@ require "digest/sha1"
 module ::DiscourseCommunityPlatform
   module Communities
     class Create
+      MAX_NAME_LENGTH = 100
       MAX_DESCRIPTION_LENGTH = 1000
 
       def self.call(user:, params:)
@@ -24,19 +25,17 @@ module ::DiscourseCommunityPlatform
         visibility = @params[:visibility].presence || "public"
         slug = ::Slug.for(@params[:slug].presence || name)
 
-        raise Discourse::InvalidParameters.new(:name) if name.blank?
+        raise Discourse::InvalidParameters.new(:name) if name.blank? || name.length > MAX_NAME_LENGTH
         raise Discourse::InvalidParameters.new(:slug) if slug.blank?
         raise Discourse::InvalidParameters.new(:description) if description&.length.to_i > MAX_DESCRIPTION_LENGTH
         raise Discourse::InvalidParameters.new(:visibility) unless Community::VISIBILITIES.include?(visibility)
+        raise Discourse::InvalidParameters.new(:slug) if Community.where("LOWER(slug) = ?", slug.downcase).exists?
 
         Community.transaction do
-          member_group = create_group(slug:, suffix: "members")
-          moderator_group = create_group(slug:, suffix: "mods")
-
-          add_creator_to_group(member_group, owner: false)
-          add_creator_to_group(moderator_group, owner: true)
-
+          member_group = create_group(slug:, suffix: "members", owner: false)
+          moderator_group = create_group(slug:, suffix: "mods", owner: true)
           category = create_category(name:, slug:, description:)
+
           configure_category_permissions(
             category:,
             visibility:,
@@ -63,6 +62,8 @@ module ::DiscourseCommunityPlatform
       private
 
       def ensure_creation_allowed!
+        raise Discourse::InvalidAccess if @user.blank?
+        raise Discourse::InvalidAccess if @user.staged? || @user.suspended? || @user.silenced?
         return if @user.admin?
 
         raise Discourse::InvalidAccess unless SiteSetting.community_platform_allow_user_community_creation
@@ -72,31 +73,27 @@ module ::DiscourseCommunityPlatform
         raise Discourse::InvalidAccess if owned_count >= SiteSetting.community_platform_max_communities_per_user
       end
 
-      def create_group(slug:, suffix:)
-        Group.create!(
-          name: group_name(slug, suffix),
-          visibility_level: Group.visibility_levels[:owners],
-          members_visibility_level: Group.visibility_levels[:members],
-          mentionable_level: Group::ALIAS_LEVELS[:nobody],
-          messageable_level: Group::ALIAS_LEVELS[:nobody],
-          public_admission: false,
-          public_exit: false,
-        )
+      def create_group(slug:, suffix:, owner:)
+        group =
+          Group.new(
+            name: group_name(slug, suffix),
+            visibility_level: Group.visibility_levels[owner ? :owners : :members],
+            members_visibility_level: Group.visibility_levels[:members],
+            mentionable_level: Group::ALIAS_LEVELS[:nobody],
+            messageable_level: Group::ALIAS_LEVELS[:nobody],
+            public_admission: false,
+            public_exit: false,
+          )
+
+        group.group_users.build(user: @user, owner:)
+        group.save!
+        group
       end
 
       def group_name(slug, suffix)
         stem = slug.tr("-", "_").gsub(/[^a-z0-9_]/, "_").first(18)
         digest = Digest::SHA1.hexdigest(slug).first(8)
         "dcp_#{stem}_#{digest}_#{suffix}"
-      end
-
-      def add_creator_to_group(group, owner:)
-        group.group_users.create!(
-          user: @user,
-          owner:,
-          notification_level: group.default_notification_level,
-        )
-        group.increment!(:user_count)
       end
 
       def create_category(name:, slug:, description:)
