ffbfafd7a1b440786e0fef1676b558ce90110309
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 15:33:36 +0300

    FEATURE: expose community creation and membership routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 DiscourseCommunityPlatform::Engine.routes.draw do
   scope "/", defaults: { format: :json } do
+    post "communities" => "communities#create"
     get "communities/:slug" => "communities#show"
+    post "communities/:slug/join" => "communities#join"
+    delete "communities/:slug/join" => "communities#leave"
   end
 end
