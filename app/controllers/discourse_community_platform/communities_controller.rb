dd1e0c4dff0fb861c96cca109d54dddcdc2665c1
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 15:33:49 +0300

    FEATURE: add community create join and leave endpoints

--- app/controllers/discourse_community_platform/communities_controller.rb
+++ app/controllers/discourse_community_platform/communities_controller.rb
@@ -4,7 +4,42 @@ module ::DiscourseCommunityPlatform
   class CommunitiesController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
+    before_action :ensure_logged_in, only: %i[create join leave]
+
+    def create
+      community = Communities::Create.call(user: current_user, params: community_params)
+
+      render_serialized(
+        community,
+        CommunitySerializer,
+        root: :community,
+        status: :created,
+      )
+    rescue ActiveRecord::RecordInvalid => error
+      render json: { errors: error.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
     def show
+      render_serialized(find_visible_community, CommunitySerializer, root: :community)
+    end
+
+    def join
+      community = Memberships::Join.call(user: current_user, community: find_visible_community)
+      render_serialized(community, CommunitySerializer, root: :community)
+    end
+
+    def leave
+      community = Memberships::Leave.call(user: current_user, community: find_visible_community)
+      render_serialized(community, CommunitySerializer, root: :community)
+    end
+
+    private
+
+    def community_params
+      params.require(:community).permit(:name, :slug, :description, :visibility)
+    end
+
+    def find_visible_community
       community =
         Community.includes(:category, :owner, :member_group, :moderator_group).find_by!(slug: params[:slug])
 
@@ -12,7 +47,7 @@ module ::DiscourseCommunityPlatform
       # own category permission model allows the current guardian to see it.
       raise Discourse::NotFound unless guardian.can_see_category?(community.category)
 
-      render_serialized(community, CommunitySerializer, root: :community)
+      community
     end
   end
 end
