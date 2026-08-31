e828bb70955f0577aac5179a7808d1d614425a10
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 15:50:29 +0300

    FIX: name emoji metadata explicitly

--- db/migrate/20260831130000_add_management_metadata_to_communities.rb
+++ db/migrate/20260831130000_add_management_metadata_to_communities.rb
@@ -4,7 +4,7 @@ class AddManagementMetadataToCommunities < ActiveRecord::Migration[7.2]
   def change
     change_table :discourse_community_platform_communities, bulk: true do |t|
       t.jsonb :rules, null: false, default: []
-      t.string :icon
+      t.string :icon_emoji, limit: 64
       t.string :banner_color, limit: 6
     end
   end
