6c897bb347d4cde12f22f618b60bfc330d55ff35
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-01 18:13:52 +0300

    Add AutoModerator execution audit model

---
+++ app/models/discourse_community_platform/automod_execution.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module ::DiscourseCommunityPlatform
+  class AutomodExecution < ActiveRecord::Base
+    self.table_name = "discourse_community_platform_automod_executions"
+
+    TRIGGERS = %w[create edit].freeze
+    OUTCOMES = %w[queued_for_review already_queued].freeze
+
+    belongs_to :community, class_name: "DiscourseCommunityPlatform::Community"
+    belongs_to :automod_rule, class_name: "DiscourseCommunityPlatform::AutomodRule", optional: true
+    belongs_to :post
+
+    validates :rule_name, presence: true
+    validates :trigger, inclusion: { in: TRIGGERS }
+    validates :outcome, inclusion: { in: OUTCOMES }
+    validates :content_sha256, presence: true, length: { is: 64 }
+  end
+end
+
+# == Schema Information
+#
+# Table name: discourse_community_platform_automod_executions
+#
+#  id              :bigint           not null, primary key
+#  community_id    :bigint           not null
+#  automod_rule_id :bigint           not null
+#  post_id         :bigint           not null
+#  rule_name       :string           not null
+#  trigger         :string           not null
+#  outcome         :string           not null
+#  content_sha256  :string(64)       not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
