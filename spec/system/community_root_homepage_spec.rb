b5ff2554cc02dc83faed186c48ae1df7e5656890
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-09 02:34:33 +0300

    STYLE: align root homepage system spec

--- spec/system/community_root_homepage_spec.rb
+++ spec/system/community_root_homepage_spec.rb
@@ -22,20 +22,20 @@ describe "Community platform root homepage" do
     visit("/")
 
     expect(page).to have_current_path("/", ignore_query: true)
-    expect(page).to have_css('.dcp-platform-shell[data-platform-section="home"]')
-    expect(page).to have_css('[data-platform-feed="home"][aria-current="page"]')
-    expect(page).to have_css('[data-platform-feed="home"][href="/"]', minimum: 1)
-    expect(page).to have_no_css('[data-platform-feed="home"][href="/home"]')
+    expect(page).to have_css(".dcp-platform-shell[data-platform-section=\"home\"]")
+    expect(page).to have_css("[data-platform-feed=\"home\"][aria-current=\"page\"]")
+    expect(page).to have_css("[data-platform-feed=\"home\"][href=\"/\"]", minimum: 1)
+    expect(page).to have_no_css("[data-platform-feed=\"home\"][href=\"/home\"]")
 
-    first('[data-platform-feed="following"]').click
+    first("[data-platform-feed=\"following\"]").click
 
     expect(page).to have_current_path("/following", ignore_query: true)
-    expect(page).to have_css('.dcp-platform-shell[data-platform-section="following"]')
+    expect(page).to have_css(".dcp-platform-shell[data-platform-section=\"following\"]")
 
-    first('[data-platform-feed="home"]').click
+    first("[data-platform-feed=\"home\"]").click
 
     expect(page).to have_current_path("/", ignore_query: true)
-    expect(page).to have_css('.dcp-platform-shell[data-platform-section="home"]')
-    expect(page).to have_css('[data-platform-feed="home"][aria-current="page"]')
+    expect(page).to have_css(".dcp-platform-shell[data-platform-section=\"home\"]")
+    expect(page).to have_css("[data-platform-feed=\"home\"][aria-current=\"page\"]")
   end
 end
