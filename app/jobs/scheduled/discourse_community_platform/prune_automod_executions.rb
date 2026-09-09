cb393a6c31b31ef3cedca6d76a825273d6be8579
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-09 17:29:44 +0300

    FIX: skip scheduled pruning when plugin disabled

--- app/jobs/scheduled/discourse_community_platform/prune_automod_executions.rb
+++ app/jobs/scheduled/discourse_community_platform/prune_automod_executions.rb
@@ -6,6 +6,8 @@ module Jobs
       every 1.day
 
       def execute(_args = {})
+        return unless SiteSetting.community_platform_enabled
+
         cutoff = ::DiscourseCommunityPlatform::AutomodExecution::RETENTION_DAYS.days.ago
         ::DiscourseCommunityPlatform::AutomodExecution.where("created_at < ?", cutoff).delete_all
       end
