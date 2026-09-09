b599015cf2e5b667360218ca4836f19f6ff0911c
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-09-09 17:30:00 +0300

    TEST: keep scheduled jobs idle when plugin disabled

---
+++ spec/jobs/community_platform_scheduled_enablement_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+RSpec.describe "Community Platform scheduled job enablement" do
+  around do |example|
+    original_enabled = SiteSetting.community_platform_enabled
+
+    begin
+      SiteSetting.community_platform_enabled = false
+      example.run
+    ensure
+      SiteSetting.community_platform_enabled = original_enabled
+    end
+  end
+
+  it "does not rebuild Explore recommendations while the plugin is disabled" do
+    allow(DiscourseCommunityPlatform::Feeds::ExploreCommunities).to receive(:rebuild_cache)
+
+    Jobs::DiscourseCommunityPlatform::RebuildExploreRecommendations.new.execute({})
+
+    expect(DiscourseCommunityPlatform::Feeds::ExploreCommunities).not_to have_received(:rebuild_cache)
+  end
+
+  it "does not rebuild activity analytics while the plugin is disabled" do
+    allow(DiscourseCommunityPlatform::Analytics::CommunityActivity).to receive(:rebuild_cache)
+
+    Jobs::DiscourseCommunityPlatform::RebuildCommunityActivityAnalytics.new.execute({})
+
+    expect(DiscourseCommunityPlatform::Analytics::CommunityActivity).not_to have_received(:rebuild_cache)
+  end
+
+  it "does not rebuild the Popular feed while the plugin is disabled" do
+    allow(DiscourseCommunityPlatform::Feeds::PopularTopics).to receive(:rebuild_cache)
+
+    Jobs::DiscourseCommunityPlatform::RebuildPopularFeed.new.execute({})
+
+    expect(DiscourseCommunityPlatform::Feeds::PopularTopics).not_to have_received(:rebuild_cache)
+  end
+
+  it "does not prune AutoModerator audit history while the plugin is disabled" do
+    allow(DiscourseCommunityPlatform::AutomodExecution).to receive(:where)
+
+    Jobs::DiscourseCommunityPlatform::PruneAutomodExecutions.new.execute({})
+
+    expect(DiscourseCommunityPlatform::AutomodExecution).not_to have_received(:where)
+  end
+end
